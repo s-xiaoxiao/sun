222e6b3fcab23ceea6baccb97dc5c00e00be532a
s-xiaoxiao <460046653@qq.com>
2020-07-16 15:26:13 +0800

    first element is not need margin left -1px

--- src/button-group.vue
+++ src/button-group.vue
@@ -23,7 +23,9 @@
     vertical-align: middle;
     > .g-button{
       border-radius:0;
-      margin-left: -1px;
+      &:not(:first-child){
+        margin-left: -1px;
+      }
       &:first-child{
         border-top-left-radius: var(--button-radius);
         border-bottom-left-radius: var(--button-radius);
