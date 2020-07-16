c4c73392b9c8bc9e33faa5c2653b5c0f9934ff1d
s-xiaoxiao <460046653@qq.com>
2020-07-16 12:08:25 +0800

    update icon style inside button.vue

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,6 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-   <g-icon v-if="icon" :name="icon"></g-icon>
+   <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
     <span class="content">
     <slot></slot>
     </span>
